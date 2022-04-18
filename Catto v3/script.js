655697609824c4b1ea512b571578365600dbd1cb
0xKirz <kirzin06@gmail.com>
2022-04-18 18:37:14 +0800

    Add files via upload

---
+++ Catto v3/script.js
@@ -0,0 +1,18 @@
+
+const links = document.querySelectorAll('.link');
+
+links.forEach(link => {
+    link.addEventListener('click', () => {
+        links.forEach(ele => ele.classList.remove('active'));
+        link.classList.add('active');
+    })
+})
+
+//toggle button for responsive
+const toggleBtn = document.querySelector('.toggle-btn');
+const linkContainer = document.querySelector('.links-container');
+
+toggleBtn.addEventListener('click', () => {
+    toggleBtn.classList.toggle('active');
+    linkContainer.classList.toggle('show');
+})
